08f74ff95d15304f911805720ed789240673b982
AkashP27 <akashphayade27@gmail.com>
2023-11-09 12:16:13 +0530

    images and utilities

---
+++ frontend/src/utils/defaultValues.js
@@ -0,0 +1,36 @@
+const defaultValues = {
+	cpp: {
+		name: "main.cpp",
+		language: "cpp",
+		value: `#include <iostream>
+
+int main() {
+    // Write C++ code here
+    std::cout << "Hello C++..!";
+
+    return 0;
+}`,
+	},
+	py: {
+		name: "main.py",
+		language: "py",
+		value: `# Write Python 3 code in this online editor and run it.
+print("Hello Python..!")`,
+	},
+	java: {
+		name: "Main.java",
+		language: "py",
+		value: `class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Java..!");
+    }
+}`,
+	},
+	js: {
+		name: "main.js",
+		language: "js",
+		value: `console.log("Hello Javascript..!");`,
+	},
+};
+
+export default defaultValues;
---
+++ frontend/src/utils/editorOptions.js
@@ -0,0 +1,21 @@
+export const optionsEditor = {
+	automaticLayout: true,
+	autoIndent: "full",
+	// contextmenu: true,
+	fontFamily: "monospace",
+	fontSize: 14,
+	readOnly: false,
+	// hideCursorInOverviewRuler: true,
+	matchBrackets: "always",
+	minimap: {
+		enabled: false,
+	},
+	scrollBeyondLastLine: false,
+	scrollbar: {
+		horizontalSliderSize: 4,
+		verticalSliderSize: 4,
+	},
+	// selectOnLineNumbers: true,
+	roundedSelection: false,
+	// cursorStyle: "line",
+};
