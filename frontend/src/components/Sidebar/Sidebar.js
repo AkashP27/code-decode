150c01ac7c5e7a56577ab8edf019818fab66d4b8
AkashP27 <akashphayade27@gmail.com>
2024-02-07 17:20:49 +0530

    refactored sidebar

--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -7,90 +7,41 @@ import javascript from "../../images/javascript.svg";
 import golang from "../../images/golang.svg";
 
 const Sidebar = ({ setLanguage }) => {
-	const [divState, setDivState] = useState({
-		activeObject: 1,
-		objects: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }],
-	});
+	const languages = [
+		{ id: 1, name: "cpp", icon: cpp },
+		{ id: 2, name: "py", icon: python },
+		{ id: 3, name: "java", icon: java },
+		{ id: 4, name: "go", icon: golang },
+		{ id: 5, name: "js", icon: javascript },
+	];
 
-	const toggleActive = (index) => {
-		setDivState({ ...divState, activeObject: divState.objects[index] });
+	const [activeLanguage, setActiveLanguage] = useState("cpp");
+
+	const handleClick = (language) => {
+		setLanguage(language);
+		setActiveLanguage(language);
 	};
 
-	const toggleActiveStyles = (index) => {
-		if (divState.objects[index] === divState.activeObject) {
-			return classes.change_lang_active;
-		}
-		return classes.change_lang_inactive;
+	const getClassName = (language) => {
+		return language === activeLanguage
+			? `${classes.change_lang_active}`
+			: `${classes.change_lang_inactive}`;
 	};
 
 	return (
 		<>
 			<div className={classes.sidebar}>
-				<div className={classes.sidebar_wrapper}>
-					<div
-						className={toggleActiveStyles(1)}
-						onClick={() => {
-							setLanguage("cpp");
-							toggleActive(1);
-						}}
-					>
-						<img src={cpp} alt="" className="icon" />
+				{languages.map((language) => (
+					<div className={classes.sidebar_wrapper}>
+						<div
+							key={language.id}
+							className={getClassName(language.name)}
+							onClick={() => handleClick(language.name)}
+						>
+							<img src={language.icon} alt="" className="icon" />
+						</div>
 					</div>
-				</div>
-
-				<div className={classes.sidebar_wrapper}>
-					<div
-						className={toggleActiveStyles(2)}
-						onClick={() => {
-							setLanguage("py");
-							toggleActive(2);
-						}}
-					>
-						<img src={python} alt="" className="icon" />
-					</div>
-				</div>
-
-				<div className={classes.sidebar_wrapper}>
-					<div
-						className={toggleActiveStyles(3)}
-						onClick={() => {
-							setLanguage("java");
-							toggleActive(3);
-						}}
-					>
-						<img src={java} alt="" className="icon" />
-					</div>
-				</div>
-
-				<div className={classes.sidebar_wrapper}>
-					<div
-						className={toggleActiveStyles(4)}
-						onClick={() => {
-							setLanguage("go");
-							toggleActive(4);
-						}}
-					>
-						<img src={golang} alt="" className="icon" />
-					</div>
-				</div>
-
-				<div className={classes.sidebar_wrapper}>
-					<div
-						className={toggleActiveStyles(5)}
-						onClick={() => {
-							setLanguage("js");
-							toggleActive(5);
-						}}
-					>
-						<img src={javascript} alt="" className="icon" />
-					</div>
-				</div>
-				{/* <label>Language</label>
-				<select value={language} onChange={(e) => setLanguage(e.target.value)}>
-					<option value="cpp">C++</option>
-					<option value="py">Python</option>
-					<option value="java">Java</option>
-				</select> */}
+				))}
 			</div>
 		</>
 	);
